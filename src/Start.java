3bfa97c39c1adba984dd9657b028cd889c667e09
Arun <arun@origin3-desktop.(none)>
2012-09-10 17:04:00 +0530

    init commit

---
+++ src/Start.java
@@ -0,0 +1,47 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: arun
+ * Date: 10/9/12
+ * Time: 3:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+
+public class Start {
+
+
+    public static class DisplayPanels{
+
+        public void SquareDisp(){
+
+            System.out.println("Called Square Display");
+        }
+        public void RectDisplay(String param){
+            int p=Integer.parseInt(param);
+
+            System.out.println("Called "+p+" Display");
+        }
+        public void RoundDisplay(){
+            System.out.println("Called Round Display");
+        }
+
+
+    }
+
+
+    public static void main(String ar[]){
+
+        System.out.println("Display Call");
+        DisplayPanels disp=new DisplayPanels();
+        disp.RoundDisplay();
+
+
+    }
+
+
+
+
+
+}
